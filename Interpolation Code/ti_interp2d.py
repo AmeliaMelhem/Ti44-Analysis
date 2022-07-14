c3dd2f3c3297eab468d640091e16d4c752e0937e
John Gallagher <jcgall517@gmail.com>
2022-07-14 18:13:51 -0400

    created a table of interpolated values for Ti44, tested for error, and plotted interpolated values against data

---
+++ Interpolation Code/ti_interp2d.py
@@ -0,0 +1,97 @@
+
+"""
+JG 
+week of 7/11-15
+using scipy to interpolate a 'function' from Ti, He, CO mass data
+then porting said function to a large matrix of values 
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp2d 
+
+# Gronow data occupies rows 0-12
+# Leung data occupies rows 13-19
+data = np.loadtxt("../Input Data/he_co_ti_dat.txt") 
+
+# 1 - Gronow
+# 2 - Leung (produces warning with too small a sample) 
+# 3 - Both
+case = 3
+
+if case == 1: 
+    a = None 
+    b = 13
+elif case == 2: 
+    a = 13
+    b = None
+elif case == 3: 
+    a = None
+    b = None
+
+he_data = data[a:b,0] 
+co_data = data[a:b,1] 
+ti_data = data[a:b,2] 
+
+
+fit_f = interp2d(he_data, co_data, ti_data, kind='linear') 
+"""
+scipy "interpolating function" object ~ non-portable
+ie performs local interpolation at each point called
+hence produces 0% error if known values are plugged in directly
+"""
+
+he_arr = np.linspace(np.amin(he_data), np.amax(he_data), 150) 
+co_arr = np.linspace(np.amin(co_data), np.amax(co_data), 150) 
+he, co = np.meshgrid(he_arr, co_arr) 
+
+fit_t = fit_f(he_arr, co_arr) 
+"""
+matrix of approx. Ti mass values
+evidently, columns are indexed by he_arr[col] 
+and rows are indexed like co_arr[row] 
+eg fit_t[3,6] == fit_f(he_arr[6], co_arr[3]) 
+"""
+
+
+def fetch_index(val, arr): 
+    """
+    finds index i of arr such that arr[i] ~ val
+    assumes that arr is sorted and ascending
+    which is true for the linspace arrays he_arr, co_arr
+    """ 
+    i = 0
+    for i in range(len(arr)): 
+        if arr[i] < val: 
+            i += 1
+        else: 
+            return i 
+
+
+"""
+Now seek to populate an array, ti_model
+with indices that allign with he_data, co_data
+to both plot and test validity of the fit (ie find %error) 
+ti_arr[i] = fit_t[fetch_index(co_data[i], co_arr), fetch_index(he_data[i], he_arr)] 
+"""
+ti_model = [] 
+i = 0
+for i in range(len(he_data)): 
+    ti_model.append(fit_t[fetch_index(co_data[i], co_arr), fetch_index(he_data[i], he_arr)])
+
+
+def fetch_err(dat_arr, mod_arr): 
+    """ 
+    returns an array with the %error between data/model
+    """
+    err = [] 
+    i = 0
+    for i in range(len(dat_arr)): 
+        err.append(100*np.abs((mod_arr[i] - dat_arr[i]) / dat_arr[i])) 
+    return err 
+
+"""
+print(fetch_err(ti_data, ti_model)) 
+print("\n") 
+print(np.mean(fetch_err(ti_data, ti_model))) 
+"""
---
+++ Interpolation Code/ti_interp2d_plots.py
@@ -0,0 +1,39 @@
+"""
+JG 
+week of 7/11-15
+Plotting interpolated function against He and core mass in various forms
+more specifically, 
+plotting the values closest to the He and CO mass values within fit_t
+using arrays created in fits.py 
+""" 
+
+from ti_interp2d import * 
+
+print("1 - Ti44 x He \n2 - Ti44 x CO \n3 - Ti(He,CO) \n4 - He x CO")
+inp = input() 
+if inp == "1": 
+   plt.scatter(he_data, ti_data, color = 'Red', label = 'Data') 
+   plt.scatter(he_data, ti_model, color = 'Blue', label = 'Model') 
+   # plt.plot(he_arr, fit_t[0,:], color = 'Blue', label = 'Fit') 
+   plt.plot(he_arr, np.zeros(len(he_arr),), 'k-') 
+   plt.title("Ti44 related to initial He mass") 
+   plt.xlabel("He Mass / Msun") 
+   plt.ylabel("Ti44 mass / Msun") 
+elif inp == "2": 
+   plt.scatter(co_data, ti_data, color = 'Red', label = 'Data') 
+   plt.scatter(co_data, ti_model, color = 'Blue', label = 'Model')
+   # plt.plot(co_arr, fit_t[:,0], color = 'Blue', label = 'Fit') 
+   plt.plot(co_arr, np.zeros(len(co_arr),), 'k-') 
+   plt.title("Ti44 related to initial core mass") 
+   plt.xlabel("Core mass / Msun") 
+   plt.ylabel("Ti44 mass / Msun") 
+elif inp == "3": 
+    plt.pcolormesh(he_arr, co_arr, fit_f(he_arr, co_arr)) 
+    plt.colorbar().set_label("Ti44 Mass", rotation=270)  
+    plt.title("Ti44 mass as function of He and Core masses") 
+    plt.xlabel("He mass / Msun") 
+    plt.ylabel("Core mass / Msun") 
+elif inp == "4": 
+    plt.scatter(co_data, he_data) # mostly just for testing
+plt.legend() 
+plt.show()
