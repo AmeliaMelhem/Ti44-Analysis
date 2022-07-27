found fatal mistake in error calculation

Interpolation Code/ti_interp2d/ti_interp2d.py
@@ -17,17 +17,17 @@ data = np.loadtxt("../../Input Data/he_co_ti_dat.txt")
 # 1 - Gronow
 # 2 - Leung (produces warning with too small a sample) 
 # 3 - Both
-case = 3
-
-if case == 1: 
-    a = None 
-    b = 13
-elif case == 2: 
-    a = 13
-    b = None
-elif case == 3: 
-    a = None
-    b = None
+dset = 3
+match dset: 
+    case 1: 
+        a = None
+        b = 13
+    case 2: 
+        a = 13
+        b = None
+    case 3: 
+        a = None
+        b = None
 
 he_data = data[a:b,0] 
 co_data = data[a:b,1] 
@@ -53,49 +53,26 @@ and rows are indexed like co_arr[row]
 eg fit_t[3,6] == fit_f(he_arr[6], co_arr[3]) 
 """
 
-
-def fetch_index(val, arr): 
-    """
-    finds index i of arr such that arr[i] ~ val
-    assumes that arr is sorted and ascending
-    which is true for the linspace arrays he_arr, co_arr
-    """ 
-    i = 0
+def fetch_ind(val, arr): 
+    # finds index of arr such that arr[i] ~ val 
+    # assuming arr is sorted and ascending  
     for i in range(len(arr)): 
-        if arr[i] < val: 
-            i += 1
+        if val > arr[i]: 
+            continue 
         else: 
-            return i 
+            return i
 
-
-"""
-Now seek to populate an array, ti_model
-with indices that allign with he_data, co_data
-to both plot and test validity of the fit (ie find %error) 
-ti_arr[i] = fit_t[fetch_index(co_data[i], co_arr), fetch_index(he_data[i], he_arr)] 
-"""
-ti_model = [] 
-i = 0
-for i in range(len(he_data)): 
-    ti_model.append(fit_t[fetch_index(co_data[i], co_arr), fetch_index(he_data[i], he_arr)])
-
-def fetch_err(dat_arr, mod_arr): 
+def fetch_err(): 
     """ 
     returns an array with the %error between data/model
     """
     err = [] 
-    i = 0
-    for i in range(len(dat_arr)): 
+    for i in range(len(he_data)): 
         temp_he = np.delete(he_data, i, 0) 
         temp_co = np.delete(co_data, i, 0) 
-        temp_ti = np.delete(co_data, i, 0) 
+        temp_ti = np.delete(ti_data, i, 0) 
         temp_fit_fn = interp2d(temp_he, temp_co, temp_ti, kind='linear')
-        # err.append(100*np.abs((ti_data[i] - temp_fit_fn(he_data[i], co_data[i])) / ti_data[i])) 
-    return err 
+        err.append(100*np.abs((ti_data[i] - temp_fit_fn(he_data[i], co_data[i])) / ti_data[i])) 
+    return np.mean(err) 
 
-print(fetch_err(ti_data, ti_model)) 
-print("\n") 
-print(np.mean(fetch_err(ti_data, ti_model))) 
-
-def ti_mass(he_mass, co_mass): 
-    return fit_t[fetch_index(co_mass, co_arr), fetch_index(he_mass, he_arr)] 
+print(fetch_err()) 