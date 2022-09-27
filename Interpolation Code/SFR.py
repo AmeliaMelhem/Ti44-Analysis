b1da0fbec58f34c78cb87d050c812b590109ccac
Amelia Melhem <amymelhem@gmail.com>
2022-09-27 09:38:13 -0400

    bug fix

--- Interpolation Code/SFR.py
+++ Interpolation Code/SFR.py
@@ -12,21 +12,22 @@ import matplotlib.pyplot as plt
 
 def findSFR(z, a, b, c, d):
     first = a * (z**2) + b*z + c
-    if first < 0:
-        first = 0
-
+    
     SFR = 10**first - d    
+    if SFR < 0:
+        SFR = 0
+
     return SFR
 
 BuldgeMass = []
 BuldgeZ = []
-for i in range(2501):    
+for i in range(251):    
     BuldgeMass.append(findSFR(i/100, -0.0406, 0.331, 0.338, 0.771))
     BuldgeZ.append(i/100)
     
 DiscMass = []
 DiscZ = []
-for i in range(2501):    
+for i in range(251):    
     DiscMass.append(findSFR(i/100, -0.0262, 0.384, -0.0842, 3.254))
     DiscZ.append(i/100)
 
@@ -38,10 +39,11 @@ plt.ylabel("Star Formation Rate (Solar Masses/year)")
 plt.xlabel("Redshit")
 plt.title("Milky Way SFR with Variable Redshift")
 
-#plt.savefig("../Plots/" + "MilkyWay_SFR")
+plt.savefig("../Plots/" + "MilkyWay_SFR")
 
 """
 Next steps:
-SFR/Star densisty in terms of time? 
+SFR/Redshift in terms of time? 
 How did Crocker et al. find it?
+http://burro.case.edu/Academics/Astr222/Cosmo/Models/age2.html
 """
