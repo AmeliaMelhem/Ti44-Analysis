f9302da976f8f9618b0a84eed32d5b4aecea3ede
Amelia Melhem <amymelhem@gmail.com>
2022-09-27 09:16:06 -0400

    typo fixing

--- Interpolation Code/SFR.py
+++ Interpolation Code/SFR.py
@@ -11,7 +11,6 @@ Created on Mon Sep 26 21:20:56 2022
 import matplotlib.pyplot as plt
 
 def findSFR(z, a, b, c, d):
-    print(z)
     first = a * (z**2) + b*z + c
     if first < 0:
         first = 0
@@ -21,13 +20,13 @@ def findSFR(z, a, b, c, d):
 
 BuldgeMass = []
 BuldgeZ = []
-for i in range(251):    
+for i in range(2501):    
     BuldgeMass.append(findSFR(i/100, -0.0406, 0.331, 0.338, 0.771))
     BuldgeZ.append(i/100)
     
 DiscMass = []
 DiscZ = []
-for i in range(251):    
+for i in range(2501):    
     DiscMass.append(findSFR(i/100, -0.0262, 0.384, -0.0842, 3.254))
     DiscZ.append(i/100)
 
@@ -35,9 +34,9 @@ plt.show()
 plt.plot(BuldgeZ, BuldgeMass, "orange", label = "Buldge SFR")
 plt.plot(DiscZ, DiscMass, "blue", label = "Disc SFR")
 plt.legend()
-plt.ylabel("Star Formation Rate")
-plt.xlabel("Galaxy Star Density")
-plt.title("Milky Way SFR with Variable Star Densities")
+plt.ylabel("Star Formation Rate (Solar Masses/year)")
+plt.xlabel("Redshit")
+plt.title("Milky Way SFR with Variable Redshift")
 
 #plt.savefig("../Plots/" + "MilkyWay_SFR")
 
