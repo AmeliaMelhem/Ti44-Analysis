78200cf017ee3b77586b9bc4f0c9d04fb8953096
Amelia Melhem <amymelhem@gmail.com>
2022-10-19 23:42:18 -0400

    increased redshift range to show full function, found modern day SFR rate successfully

--- Interpolation Code/SFR.py
+++ Interpolation Code/SFR.py
@@ -10,6 +10,8 @@ Created on Mon Sep 26 21:20:56 2022
 
 import matplotlib.pyplot as plt
 
+zMax = 13
+
 def findSFR(z, a, b, c, d):
     first = a * (z**2) + b*z + c
     
@@ -21,19 +23,19 @@ def findSFR(z, a, b, c, d):
 
 BuldgeMass = []
 BuldgeZ = []
-for i in range(251):    
-    BuldgeMass.append(findSFR(i/100, -0.0406, 0.331, 0.338, 0.771))
+for i in range(zMax*100+1):    
+    BuldgeMass.append(findSFR(i/100, -0.0262, 0.384, -0.0842, 3.254))
     BuldgeZ.append(i/100)
     
 DiscMass = []
 DiscZ = []
-for i in range(251):    
-    DiscMass.append(findSFR(i/100, -0.0262, 0.384, -0.0842, 3.254))
+for i in range(zMax*100+1):    
+    DiscMass.append(findSFR(i/100, -0.0406, 0.331, 0.338, 0.771))
     DiscZ.append(i/100)
 
 plt.show()
-plt.plot(BuldgeZ, BuldgeMass, "orange", label = "Buldge SFR")
-plt.plot(DiscZ, DiscMass, "blue", label = "Disc SFR")
+plt.plot(BuldgeZ, BuldgeMass, "orange", label = "Buldge 'SFR'")
+plt.plot(DiscZ, DiscMass, "blue", label = "Disc 'SFR'")
 plt.legend()
 plt.ylabel("Star Formation Rate (Solar Masses/year)")
 plt.xlabel("Redshift")
@@ -41,9 +43,14 @@ plt.title("Milky Way SFR with Variable Redshift")
 
 plt.savefig("../Plots/" + "MilkyWay_SFR")
 
+print(findSFR(0, -0.0406, 0.331, 0.338, 0.771))
+
 """
 Next steps:
 SFR/Redshift in terms of time? 
 How did Crocker et al. find it?
+
+For present time, Crocker used z = 0 (or 8.16), to find an SFR of 1.4 M/yr
+
 http://burro.case.edu/Academics/Astr222/Cosmo/Models/age2.html
 """
