90e1df84eb50a82fc45709b3db1bc8b109614605
Amelia Melhem <amymelhem@gmail.com>
2023-02-09 14:36:04 -0500

    extend WD range for He Mass estimation

--- Interpolation Code/FindHeMass.py
+++ Interpolation Code/FindHeMass.py
@@ -12,18 +12,19 @@ Created on Wed Jun 15 15:22:20 2022
 ## Assumes Z = 0.01 and that WD core mass is equal to total mass
 ## Also assumes that the buffer mass of the He is equal to the total He mass
 
+## 2/9: MBuff function altered to remove mass limits on WD -AM
 
 import numpy as np
 import pandas as pd
 
-file = 1 # 0 for first file(aa), 1 for second(ag)
+file = 0 # 0 for first file(aa), 1 for second(ag)
 
 if file == 0:
     dataset = np.loadtxt("../Input Data/SeBa_aa_020418_production_run_wdwd_bob.data")
-    fileName = 'SeBa_aa_with_He.txt'
+    fileName = 'Full_SeBa_aa_with_He.txt'
 else: 
     dataset = dataset = np.loadtxt("../Input Data/SeBa_ag_020418_production_run_wdwd_bob.data")
-    fileName = 'SeBa_ag_with_He.txt'
+    fileName = 'Full_SeBa_ag_with_He.txt'
 
 
 
@@ -80,14 +81,14 @@ def MBuff(mass, a, b, c, d, e): # Too specific to be generalized -AM
 
     """
     
-    
-    
-    if mass < d or mass > e: #Writes '-' if the mass is not within the acccepted range
-        MBuff = '-'   
-        
-    else:
-        MBuff = 10**(a + b*np.log10(mass) + c*(np.log10(mass)**2)) # Equation 9
-        
+    #if mass < d or mass > e: #Writes '-' if the mass is not within the acccepted range
+    #    MBuff = '-'   
+    #    
+    #else:
+    #    MBuff = 10**(a + b*np.log10(mass) + c*(np.log10(mass)**2)) # Equation 9
+
+    MBuff = 10**(a + b*np.log10(mass) + c*(np.log10(mass)**2))
+            
     return MBuff
 
 
@@ -149,31 +150,6 @@ pdDataset = removeNull(pdDataset)
 # Save as new txt file. Change header to False to be numpy compatible or add
 # '#' to the front of the first line of txt.
 
-# with open('./' + fileName, 'w') as f:
-#     dfAsString = pdDataset.to_string(header=True, index=False)
-#     f.write(dfAsString)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+with open('./' + fileName, 'w') as f:
+    dfAsString = pdDataset.to_string(header=True, index=False)
+    f.write(dfAsString)
