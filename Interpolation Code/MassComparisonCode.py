e6b222242a15fc15c4a373b78cd2479c65ccc26a
= <=>
2023-04-07 14:46:26 -0400

    combined aa and ag for WD mass v He shell mass

--- Interpolation Code/MassComparisonCode.py
+++ Interpolation Code/MassComparisonCode.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@authors: Alexis Petty
+@authors: Alexis Petty & Amelia Melhem
 
 Creates plots to compare to figure 9 in 
 https://articles.adsabs.harvard.edu/pdf/2006MNRAS.371..263L 
@@ -27,25 +27,30 @@ else:
 
 Mwd1=np.log10(data[:,3])
 He_min_wd1=np.log10(data[:,7])
-plt.plot(Mwd1, He_min_wd1,"ro")
-
 He_max_wd1=np.log10(data[:,9])
-plt.plot(Mwd1, He_max_wd1,"b*")
+
+sorted_pairs = sorted(zip(Mwd1, He_min_wd1, He_max_wd1))
+Mwd1, He_min_wd1, He_max_wd1 = [list(i) for i in zip(*sorted_pairs)]
+
+plt.plot(Mwd1, He_min_wd1,"k-", label="He Mass min")
+plt.plot(Mwd1, He_max_wd1,"k--", label="He Mass max")
+
 
 Mwd2=np.log10(data[:,4])
 He_min_wd2=np.log10(data[:,8])
-plt.plot(Mwd2, He_min_wd2,"ro")
-
 He_max_wd2=np.log10(data[:,10])
-plt.plot(Mwd2, He_max_wd2,"b*")
+
+sorted_pairs = sorted(zip(Mwd2, He_min_wd2, He_max_wd2))
+Mwd2, He_min_wd2, He_max_wd2 = [list(i) for i in zip(*sorted_pairs)]
+
+plt.plot(Mwd2, He_min_wd2,"k-")
+plt.plot(Mwd2, He_max_wd2,"k--")
 
 
+plt.legend()
+plt.ylabel('Log He Mass ($M_{\odot}$)')
+plt.xlabel('Log WD Mass ($M_{\odot}$)')
 
-plt.title('WD-Mass Vs. He-Mass')
-plt.legend(['W1min','W1max', 'W2min', 'W2max'])
-plt.ylabel('Log He Mass')
-plt.xlabel('Log WD Mass')
-
-plt.savefig(fileName)
+#plt.savefig(fileName)
 
 plt.show()
