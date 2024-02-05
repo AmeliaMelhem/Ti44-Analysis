e55ff1683ca89435e1b8a7085a5ed0880d6a68b3
Amelia <amymelhem@gmail.com>
2024-02-05 09:20:05 -0500

    Added 0.0002 & 0.02 metalicty data

--- Interpolation Code/FindHeMass.py
+++ Interpolation Code/FindHeMass.py
@@ -15,17 +15,19 @@ Created on Wed Jun 15 15:22:20 2022
 
 ## 2/9: MBuff function altered to remove mass limits on WD -AM
 
+## 2/4/24: Looked at change in He buffer mass for varying metalicty assumptions -AM
+
 import numpy as np
 import pandas as pd
 
-file = 0 # 0 for first file(aa), 1 for second(ag)
+file = 1 # 0 for first file(aa), 1 for second(ag)
 
 if file == 0:
-    dataset = np.loadtxt("./Input Data/SeBa_aa_020418_production_run_wdwd_bob.data")
-    fileName = 'Full_He_aa_with_Zenati.txt'
+    dataset = np.loadtxt("../Input Data/SeBa_aa_020418_production_run_wdwd_bob.data")
+    fileName = '../Output Data/Full_He_aa_with_Zenati.txt'
 else: 
-    dataset = np.loadtxt("./Input Data/SeBa_ag_020418_production_run_wdwd_bob.data")
-    fileName = 'Full_He_ag_with_Zenati.txt'
+    dataset = np.loadtxt("../Input Data/SeBa_ag_020418_production_run_wdwd_bob.data")
+    fileName = '../Output Data/Full_He_ag_with_Zenati.txt'
 
 
 
@@ -55,25 +57,34 @@ pdDataset = pd.DataFrame(dataset, columns = ['id', 'DWD_formation_time(Myr)',
                                              'time_of_merger(Myr)', 'Mwd1(Msun)', 
                                              'Mwd2(Msun)', 'type_wd1', 'type_wd2' ])
 # Initalize new columns for calculated data
-pdDataset['He_min_wd1(Msun)'] = 0
-pdDataset['He_min_wd2(Msun)'] = 0
-pdDataset['He_max_wd1(Msun)'] = 0
-pdDataset['He_max_wd2(Msun)'] = 0
+pdDataset['He_min_wd1(Msun)_Z0002'] = 0
+pdDataset['He_min_wd2(Msun)_Z0002'] = 0
+pdDataset['He_max_wd1(Msun)_Z0002'] = 0
+pdDataset['He_max_wd2(Msun)_Z0002'] = 0
 
+pdDataset['He_min_wd1(Msun)_Z01'] = 0
+pdDataset['He_min_wd2(Msun)_Z01'] = 0
+pdDataset['He_max_wd1(Msun)_Z01'] = 0
+pdDataset['He_max_wd2(Msun)_Z01'] = 0
+
+pdDataset['He_min_wd1(Msun)_Z02'] = 0
+pdDataset['He_min_wd2(Msun)_Z02'] = 0
+pdDataset['He_max_wd1(Msun)_Z02'] = 0
+pdDataset['He_max_wd2(Msun)_Z02'] = 0
 
 ## Function to find the buffer mass given WD mass and variables from Table 6
 ## Mass assumed to be given in solar masses
 
 
-def MBuff(mass, a, b, c, d, e): # Too specific to be generalized -AM
+def MBuff(mass, LM_T6_vars):
     """
-    [OUTDATED DESCRIPTION]
-    Uses Equation 9 to estimate He Mass given the WD mass and parameters
+    First uses Zenati et al. to estimate He buffer mass given WD mass. 
+    If out of range, then uses equation 9 and table 6 from Lawlor and MacDonald (2006)
     
     Parameters
     ----------
     mass : Core Mass of the WD
-    a, b, c : Given by the table
+    a, b, c : Given by table 6
     d, e : Min and Max allowed input WD masses. returns '-' if the input is 
     outside this range
 
@@ -90,43 +101,51 @@ def MBuff(mass, a, b, c, d, e): # Too specific to be generalized -AM
         MBuff = mass - 0.3    
     
     else:
-        MBuff = 10**(a + b*np.log10(mass) + c*(np.log10(mass)**2)) # Equation 9 Lawlor and MacDonald
+        MBuff = 10**(LM_T6_vars[0] + LM_T6_vars[1]*np.log10(mass) + LM_T6_vars[2]*(np.log10(mass)**2)) # Equation 9 Lawlor and MacDonald
 
     return MBuff
 
 
 ## Uses MBuff to assign buffer mass values in pdDataset 
-## Assumes Z = 0.01
 
 def assignMass(df): 
+
+    # Z = 0.0002    
+    z0_0002_min =   [-3.4083, -8.0814, -7.4303,  0.58, 1.22]
+    z0_0002_max =   [-2.9816, -8.0142, -10.9891, 0.57, 1.23]
+
+    # Z = 0.01    
+    z0_01_min =     [-3.3243, -6.7603, -3.0043,  0.52, 0.94]
+    z0_01_max =     [-2.9129, -6.1056, -5.0948,  0.52, 0.93]
+
+    # Z = 0.02    
+    z0_02_min =     [-3.3106, -7.1480, -4.4195,  0.51, 0.97]
+    z0_02_max =     [-3.0248, -7.5528, -10.5975, 0.57, 0.97]
+
+
+    # All Table 6 variables, to be easily iterable
+    zList = [z0_0002_min, z0_0002_max, 
+             z0_01_min  , z0_01_max, 
+             z0_02_min  , z0_02_max]
     
     for x in range(len(df)):
         row = x
         
-        ## MBuff min 
-        a = -3.3243
-        b = -6.7603
-        c = -3.0043
-        d = 0.52
-        e = 0.94
-        mass = df.iat[row, 3] #WD 1
-        df.iat[row, 7] = MBuff(mass, a, b, c, d, e)
-    
-        mass = df.iat[row, 4] #WD 2
-        df.iat[row, 8] = MBuff(mass, a, b, c, d, e)
-    
-        ## MBuff max 
-        a = -2.9129
-        b = -6.1056
-        c = -5.0948
-        d = 0.52
-        e = 0.93 
-    
-        mass = df.iat[row, 3] #WD 1
-        df.iat[row, 9] = MBuff(mass, a, b, c, d, e)
-    
-        mass = df.iat[row, 4] #WD 2
-        df.iat[row, 10] = MBuff(mass, a, b, c, d, e)
+        for i in range(3):
+
+            ## MBuff min 
+            mass = df.iat[row, 3] #WD 1
+            df.iat[row, 4*i+7] = MBuff(mass, zList[i*2])
+        
+            mass = df.iat[row, 4] #WD 2
+            df.iat[row, 4*i+8] = MBuff(mass, zList[i*2])
+        
+            ## MBuff max     
+            mass = df.iat[row, 3] #WD 1
+            df.iat[row, 4*i+9] = MBuff(mass, zList[i*2+1])
+        
+            mass = df.iat[row, 4] #WD 2
+            df.iat[row, 4*i+10] = MBuff(mass, zList[i*2+1])
 
     return df
 
@@ -136,12 +155,13 @@ def assignMass(df):
 def removeNull(df): # Could be generalized if useful but probably not -AM
     xList = []
     for x in range(len(df)):
-        row = x
-        if (df.iat[row, 7] == '-' and # change all 'and' to 'or' to only
-            df.iat[row, 8] == '-' and # store information when all 4 buffer
-            df.iat[row, 9] == '-' and # masses can be found
-            df.iat[row, 10] == '-'):
-                xList.append(x)
+        for i in range(3):
+            row = x
+            if (df.iat[row, 4*i+7] == '-' and # change all 'and' to 'or' to only
+                df.iat[row, 4*i+8] == '-' and # store information when all 4 buffer
+                df.iat[row, 4*i+9] == '-' and # masses can be found
+                df.iat[row, 4*i+10] == '-'):
+                    xList.append(x)
                 
     df = df.drop(xList)
     
@@ -153,6 +173,6 @@ pdDataset = removeNull(pdDataset)
 # Save as new txt file. Change header to False to be numpy compatible or add
 # '#' to the front of the first line of txt.
 
-with open('./' + fileName, 'w') as f:
+with open(fileName, 'w') as f:
     dfAsString = pdDataset.to_string(header=True, index=False)
     f.write(dfAsString)
--- Interpolation Code/MassComparisonCode.py
+++ Interpolation Code/MassComparisonCode.py
@@ -16,36 +16,41 @@ import numpy as np
 file = 0 # 0 for first file(aa), 1 for second(ag)
 
 if file == 0:
-    data = np.loadtxt("./Full_He_aa_with_Zenati.txt")    
+    data = np.loadtxt("../Output Data/Full_He_aa_with_Zenati.txt")    
     fileName = 'Combined_Mass_Comparison'
 else: 
-    data = np.loadtxt("./Full_He_ag_with_Zenati.txt")
+    data = np.loadtxt("../Output Data/Full_He_ag_with_Zenati.txt")
     fileName = 'Combined_Mass_Comparison'
 
 
 
-Mwd1=np.log10(data[:,3])
-He_min_wd1=np.log10(data[:,7])
-He_max_wd1=np.log10(data[:,9])
 
-sorted_pairs = sorted(zip(Mwd1, He_min_wd1, He_max_wd1))
-Mwd1, He_min_wd1, He_max_wd1 = [list(i) for i in zip(*sorted_pairs)]
-
-plt.plot(Mwd1, He_min_wd1,"k-", label="He Mass min")
-plt.plot(Mwd1, He_max_wd1,"k--", label="He Mass max")
+colorList = ["g-","g--",
+             "b-","b--",
+             "k-","k--"]
+labelList = ["He Mass min Z=0.0002" ,"He Mass max Z=0.0002",
+             "He Mass min Z=0.01"   ,"He Mass max Z=0.01",
+             "He Mass min Z=0.02"   ,"He Mass max Z=0.02"]
 
 
-Mwd2=np.log10(data[:,4])
-He_min_wd2=np.log10(data[:,8])
-He_max_wd2=np.log10(data[:,10])
+for i in range(3):
+    Mwd2=np.log10(data[:,4])
+    Mwd1=np.log10(data[:,3])
+    He_min_wd1=np.log10(data[:,4*i+7])
+    He_min_wd2=np.log10(data[:,4*i+8])
+    He_max_wd1=np.log10(data[:,4*i+9])
+    He_max_wd2=np.log10(data[:,4*i+10])
 
-sorted_pairs = sorted(zip(Mwd2, He_min_wd2, He_max_wd2))
-Mwd2, He_min_wd2, He_max_wd2 = [list(i) for i in zip(*sorted_pairs)]
+    sorted_pairs_1 = sorted(zip(Mwd1, He_min_wd1, He_max_wd1))
+    Mwd1, He_min_wd1, He_max_wd1 = [list(i) for i in zip(*sorted_pairs_1)]
+    sorted_pairs_2 = sorted(zip(Mwd2, He_min_wd2, He_max_wd2))
+    Mwd2, He_min_wd2, He_max_wd2 = [list(i) for i in zip(*sorted_pairs_2)]
 
-plt.plot(Mwd2, He_min_wd2,"k-")
-plt.plot(Mwd2, He_max_wd2,"k--")
-#plt.plot(-0.31852,-1.498498, "bo")
 
+    plt.plot(Mwd1, He_min_wd1,colorList[2*i],   label=labelList[2*i])
+    plt.plot(Mwd1, He_max_wd1,colorList[2*i+1], label=labelList[2*i+1])
+    plt.plot(Mwd2, He_min_wd2,colorList[2*i])
+    plt.plot(Mwd2, He_max_wd2,colorList[2*i+1])
 
 plt.legend()
 plt.ylabel('Log He Mass ($M_{\odot}$)')
