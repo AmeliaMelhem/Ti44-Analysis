64fc99b31bc8364fd254fcf3a882f6eabab3663d
Amelia <amymelhem@gmail.com>
2024-10-13 13:05:13 -0400

    updated metallicites comparison

--- Interpolation Code/MW_rate_metallicities_comparison.py
+++ Interpolation Code/MW_rate_metallicities_comparison.py
@@ -35,36 +35,57 @@ Relative_difference_01   = (MW_01_Rate - MW_01_Rate)/MW_01_Rate
 #13871 is the zero-pt index
 
 
+plt.rc('axes', labelsize=18)
+
 # Relative Differences
+# plt.figure(figsize=(12,   6))
+# plt.subplot(1, 2, 1)
 
-plt.subplot(1, 2, 1)
-
+plt.plot(times, Relative_difference_0002,    label='Relative Difference Z = 0.0002')
 plt.plot(times, Relative_difference_01, 'k', label='Relative Difference Z = 0.01 (baseline)')
-plt.plot(times, Relative_difference_0002, label='Relative Difference Z = 0.0002')
-plt.plot(times, Relative_difference_02,   label='Relative Difference Z = 0.02')
+plt.plot(times, Relative_difference_02,      label='Relative Difference Z = 0.02')
 
 plt.ylabel("Relative Difference")
 plt.xlabel("Time (Myr)")
 plt.legend()
-plt.title("Percent difference of Ti44 production over time")
+# plt.title("Percent difference of $^{44}$Ti production over time")
+
+# plt.savefig("./Plots/Metallicity/Fulla.png")
+
+plt.xlim(364,368)
+plt.savefig("./Plots/Metallicity/ZoomIna.png")
+
+
+# plt.xlim(1000,3000)
+# plt.ylim(-0.002,0.002)
+# plt.savefig("./Plots/Metallicity/Typical.png")
 
 #==============================================================================================#
 
 # Ti44 Rates
 
-plt.subplot(1, 2, 2)
+# plt.subplot(1, 2, 2)
 
+plt.clf()
 plt.yscale("log")
+plt.plot(times, MW_0002_Rate,    label = 'Z = 0.0002')
 plt.plot(times, MW_01_Rate, 'k', label = 'Z = 0.01')
-plt.plot(times, MW_0002_Rate, label = 'Z = 0.0002')
-plt.plot(times, MW_02_Rate, label = 'Z = 0.02')
+plt.plot(times, MW_02_Rate,      label = 'Z = 0.02')
 
-plt.ylabel("Ti44 Rate")
+plt.ylabel("Specific $^{44}$Ti Rate")
 plt.xlabel("Time (Myr)")
-plt.title("Ti44 production vs time")
-plt.xlim(0,M)
+# plt.title("$^{44}$Ti production vs time")
 plt.legend()
-plt.show()  
+# plt.show()  
 
 
+plt.xlim(366,366.4)
+plt.ylim(4*10**-8, 9*10**-8)
+plt.tight_layout(h_pad=1)
+plt.savefig("./Plots/Metallicity/ZoomInb.png")
+
+
+# plt.xlim(0,M)
+# # plt.tight_layout(h_pad=2)
+# plt.savefig("./Plots/Metallicity/Fullb.png")
 
